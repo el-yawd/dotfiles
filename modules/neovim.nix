197ad9b8e191b17f56d2900e8e1d5fa3a85d2a13
Diego Reis <reisdiego144@gmail.com>
2026-02-14 17:45:49 -0300

    Change neovim to default config
    I'll use lazyvim in a non-reproducible way, sorry functional gods

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,16 +1,11 @@
 {
-  lazyvim-nix,
   ...
 }:
 {
-  home-manager.users.yawd = {
-    imports = [ lazyvim-nix.homeManagerModules.default ];
-
-    programs.lazyvim = {
-      enable = true;
-      extras = {
-        lang.nix.enable = true;
-      };
-    };
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    viAlias = true;
   };
 }
